e2889c66d1d2dd36816a0dd2c64ea1b81ba00c57
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-11-09 15:44:23 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
+
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <React path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> {/* ✅ fixed */}
       </Routes>
     </BrowserRouter>
   );
