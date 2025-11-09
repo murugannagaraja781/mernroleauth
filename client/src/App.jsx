696dc9d663fab1eb2e3c524b2792a0cb01e99ed0
murugannagaraja781 <murugannagaraja781@gmail.com>
2025-11-09 16:09:15 +0530

    clear

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Home from "./pages";
+import Home from "./pages/Home";
 
 export default function App() {
   return (
